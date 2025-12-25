4bdd23a892e66ffa65155f20cdcd82a14ac6f9a0
Maks Hrech <grechemax1@gmail.com>
2025-12-25 13:59:57 +0100

    Separate tax tests

--- src/pages/taxes.page.ts
+++ src/pages/taxes.page.ts
@@ -1,10 +1,17 @@
-// import { expect } from '@playwright/test';
 import { BasePage } from './base-page';
-// import { logStep } from '../utils/customUtils';
 
 export class TaxesPage extends BasePage {
-    public readonly totalTax = this.page.locator('.tax-total span.summary-value');
+    public readonly filterTotalTax = this.page.locator('.filter-summary .tax-total');
     public readonly taxTableContainer = this.page.locator('.taxes-table-container');
+    public readonly tableTotalTax = this.page.locator('.summary-stats .quarter-totals .total-item:nth-child(2)');
+
+    public async assertFilterTotalTaxVisible(): Promise<void> {
+        await this.assertElementVisible(this.filterTotalTax);
+    }
+
+    public async assertFilterTotalTaxNotVisible(): Promise<void> {
+        await this.assertElementNotVisible(this.filterTotalTax);
+    }
 
     public async assertTaxTableVisible(): Promise<void> {
         await this.assertElementVisible(this.taxTableContainer);
@@ -15,28 +22,7 @@ export class TaxesPage extends BasePage {
     }
 
     public async getTotalTaxText(): Promise<string> {
-        await this.totalTax.waitFor({ state: 'visible', timeout: 5000 });
-        return (await this.totalTax.textContent()) || '';
+        await this.tableTotalTax.waitFor({ state: 'visible', timeout: 5000 });
+        return (await this.tableTotalTax.textContent()) || '';
     }
-
-    // public async assertModalVisible(): Promise<void> {
-    //     await expect(this.loginModal).toBeVisible();
-    //     await expect(this.modalTitle).toBeVisible();
-    // }
-
-    // public async fillLoginForm(email: string, password: string): Promise<void> {
-    //     await this.emailInput.fill(email);
-    //     await this.passwordInput.fill(password);
-    //     // await logStep('Filled login form');
-    // }
-
-    // public async submitLogin(): Promise<void> {
-    //     await this.submitButton.click();
-    //     // Assert success/error (e.g., await expect(this.page).toHaveURL(/dashboard/)) if login redirects; else check message
-    // }
-
-    // public async assertPageContent(): Promise<void> {
-    //     await expect(this.pageHeading).toBeVisible();
-    //     // Add more: e.g., expect specific tax info text
-    // }
 }
